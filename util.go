7d11a23c68c355c17099b7fb1dc0428a7581efe1
chenlianghong <clh021@gmail.com>
2022-09-14 14:03:23 +0800

    cache new case

--- util.go
+++ util.go
@@ -4,7 +4,9 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"os/user"
 	"path/filepath"
+	"strconv"
 	"syscall"
 )
 
@@ -28,6 +30,30 @@ func doCmd(t Step, envs *[]string) error {
 	cmd := exec.Command(client)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Pdeathsig: syscall.SIGINT, //如果主进程退出，则将 SIGINT 发送给子进程
+	}
+	cmd.Env = *envs
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func doCmdUser(t Step, envs *[]string) error {
+	client := filepath.Join(GetProgramPath(), t.Exec)
+	log.Println("[ cmd]:" + client)
+	cmd := exec.Command(client)
+	// 发送信号关闭子进程
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Pdeathsig: syscall.SIGINT, //如果主进程退出，则将 SIGINT 发送给子进程
+	}
+	// 设置具体运行用户
+	user, err := user.Lookup("nobody")
+	if err == nil {
+		log.Printf("uid=%s,gid=%s", user.Uid, user.Gid)
+
+		uid, _ := strconv.Atoi(user.Uid)
+		gid, _ := strconv.Atoi(user.Gid)
+
+		cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)}
 	}
 	cmd.Env = *envs
 	cmd.Stdout = os.Stdout
